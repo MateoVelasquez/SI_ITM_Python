8299de0bca047645b5dc164205616d1e317a107d
MateoVelasquez <mateo10velasquez@hotmail.com>
2020-07-18 17:44:40 -0500

    Eliminadas fuciones incompletas

--- alg_gen_sim/__init__.py
+++
@@ -1,8 +0,0 @@
-__author__ = "Mateo Velásquez Molina"
-__copyrigth__ = "2019 -2020 Mateo Velasquez. Todos los derechos"
-__credits__ = ["Mateo Velasquez Molina", "Alejandro Gutierrez Fuentes"]
-__license__ = "Creative Commons Attribution License"
-__version__ = "20200111"
-__mainteiner__ = "Mateo Velásquez Molina"
-__email__ = "mateo10velasquez@hotmail.com"
-__status__ = "Development"
